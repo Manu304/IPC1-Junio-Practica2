17ea1cf12ec6bdc4b0187f98a342c9036579482d
Manuel Rojas <manuerjs70@gmail.com>
2021-06-12 13:52:49 -0600

    Agregue algoritmo para solicitud de Strings

--- src/Blockbuster.java
+++ src/Blockbuster.java
@@ -7,50 +7,67 @@ public class Blockbuster {
     public static void main(String[] args) {
         Blockbuster programa = new Blockbuster();
     }
-    final String [] PRONOMBRES = {"el", "la"};
-    Scanner scanner = new Scanner(System.in);
-    String [] nombreClientes, nombrePelis,categoriaPelis;
-    int [][] peliculasPrestadas;
-    int [] telefonoClientes, idClientes, idPelis, anioPelis;
-    boolean [] clientePresta, peliDisponible;
 
-    public Blockbuster(){
+    final String[] PRONOMBRES = { "el", "la" };
+    Scanner scanner = new Scanner(System.in);
+    String[][] clientes, peliculas;
+    int [][] prestamoPeliculas;
+    //String[] nombreClientes, nombrePelis, categoriaPelis;
+    //int[][] peliculasPrestadas, idTelefonoClientes, idAnioPelis;
+    //int[] telefonoClientes, idClientes, idPelis, anioPelis;
+    //boolean[] clientePresta, peliDisponible;
+
+    public Blockbuster() {
+        final int CANTIDAD_INICIAL = 30;
         menu();
 
     }
-    public int pedirNumero(String solicitud, boolean femPronombre){
+    
+
+    public int pedirNumero(String solicitud, boolean femPronombre) {
         int numero = 0;
         String pronombre = PRONOMBRES[1];
         if (!femPronombre) {
             pronombre = PRONOMBRES[0];
         }
         do {
-            System.out.print("Ingrese " + pronombre + " " + solicitud + ": ");
-            int ingresado = scanner.nextInt();
-            if (ingresado < 0) {
-                System.out.println("\nNo puede ingresar un valor negativo");
-            }
-            numero = ingresado;
+            try {
+                System.out.print("Ingrese " + pronombre + " " + solicitud + ": ");
+                int ingresado = scanner.nextInt();
+                if (ingresado < 0) {
+                    System.out.println("\nNo puede ingresar un valor negativo");
+                }
+                numero = ingresado;
 
+            } catch (InputMismatchException e) {
+                System.out.println("ERROR. Debes de ingresar un numero");
+                scanner.nextLine();
+                numero = -1;
+            }
         } while (numero < 0);
         return numero;
     }
-    /*
-    public String pedirString(String solicitud, boolean femPronombre){
-        String palabra, fem = "la";
 
+    public String pedirString(String solicitud, boolean femPronombre) {
+        String pronombre = PRONOMBRES[1];
+        if (!femPronombre) {
+            pronombre = PRONOMBRES[0];
+        }
+        System.out.print("Ingrese " + pronombre + " " + solicitud + ": ");
+        String palabra = scanner.nextLine();
+        palabra = scanner.nextLine();
+        return palabra;
     }
-    */
 
     public void menu() {
         boolean salir = false;
         int opcion = 0;
-        while(!salir){
-            //mostrarle al usuario las opciones
+        while (!salir) {
+            // mostrarle al usuario las opciones
             System.out.println("---------MENU PRINCIPAL---------");
-            System.out.println("    \n1. Prestar pelicula" + "    \n2. Devolver pelicula" + "    \n3. Mostrar peliculas" 
-                            + "    \n4. Ingresar peliculas" + "    \n5. Ordenar peliculas" + "    \n6. Ingresar nuevos clientes"
-                            + "    \n7. Mostrar clientes" + "    \n8. Reportes" + "    \n9. Salir\n");
+            System.out.println("    \n1. Prestar pelicula" + "    \n2. Devolver pelicula" + "    \n3. Mostrar peliculas"
+                    + "    \n4. Ingresar peliculas" + "    \n5. Ordenar peliculas" + "    \n6. Ingresar nuevos clientes"
+                    + "    \n7. Mostrar clientes" + "    \n8. Reportes" + "    \n9. Salir\n");
             try {
                 System.out.print("Ingrese una opcion: ");
                 opcion = scanner.nextInt();
@@ -66,8 +83,7 @@ public class Blockbuster {
                         break;
                     case 4:
                         System.out.println("quiere ingresar pelis");
-                        int random = pedirNumero("año", false);
-                        System.out.println(random);
+                        
                         break;
                     case 5:
                         System.out.println("Quiere ordenar las pelis");
@@ -89,12 +105,12 @@ public class Blockbuster {
                         System.out.println("Ups. Esa opción no existe.");
                         break;
                 }
-                
+
             } catch (InputMismatchException e) {
                 System.out.println("\nERROR. Debes de ingresar un numero\n");
-                scanner.next();
+                scanner.nextLine();
             }
-        }    
+        }
     }
 
 }
